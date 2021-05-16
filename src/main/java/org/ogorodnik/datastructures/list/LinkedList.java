cfe06faf54a752e2558c679b16b7f6bba112a754
Oleksandr Ohorodnik <persyak@ukr.net>
2021-05-16 22:51:45 +0300

    commit gitignore

---
+++ src/main/java/org/ogorodnik/datastructures/list/LinkedList.java
@@ -0,0 +1,213 @@
+package org.ogorodnik.datastructures.list;
+
+import java.util.Arrays;
+
+public class LinkedList implements List{
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void add(Object value){
+        add(value, size);
+    }
+
+    public void add(Object value, int index){
+        validateIndexForAdd(index);
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else if(index == 0){
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else if(index == size){
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        } else {
+            Node current = head.next;
+            int pointer = 1;
+            while(pointer < index){
+                current = current.next;
+                pointer++;
+            }
+            Node temp = current.prev;
+            newNode.prev = temp;
+            newNode.next = current;
+            current.prev = newNode;
+            temp.next = newNode;
+        }
+        size++;
+    }
+
+    public Object remove(int index){
+        validateIndex(index);
+        Object removed;
+        if(head == tail){
+            removed = head.value;
+            head = null;
+            tail = null;
+        } else if (index == 0) {
+            removed = head.value;
+            head = head.next;
+            head.prev = null;
+        } else if(index == size-1){
+            removed = tail.value;
+            tail = tail.prev;
+            tail.next = null;
+        }
+        else {
+            Node current = head.next;
+            int pointer = 1;
+            while (pointer < index) {
+                current = current.next;
+                pointer++;
+            }
+            removed = current.value;
+            Node prev = current.prev;
+            Node next = current.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+        size--;
+        return removed;
+    }
+
+    public Object get(int index){
+        validateIndex(index);
+        Node current = head;
+        int pointer = 0;
+        while (pointer < index) {
+            current = current.next;
+            pointer++;
+        }
+        return current.value;
+    }
+
+    public Object set(Object value, int index){
+        validateIndex(index);
+        Node current = head;
+        int pointer = 0;
+        while (pointer < index) {
+            current = current.next;
+            pointer++;
+        }
+        current.value = value;
+        return current.value;
+    }
+
+    public void clear(){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty");
+        }
+        else{
+            while (head != tail) {
+                head = head.next;
+                head.prev = null;
+            }
+            head = null;
+            tail = null;
+            size = 0;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean contains(Object value){
+        return indexOf(value) !=-1;
+    }
+
+    public int indexOf(Object value){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty");
+        }
+        else{
+            int index = 0;
+            Node current = head;
+            while(current.next != null){
+//TODO: лучше на индекс ориентироваться, или на следующий элемент, когда итерируемся по листу?
+// если количество элементов соответствует size, то должно быть без разницы;
+                if (current.value.equals(value)){
+                    return index;
+                } else{
+                    current = current.next;
+                    index ++;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object value){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty");
+        }
+        else{
+            int index = size-1;
+            Node current = tail;
+            while(index>=0){
+                if (current.value.equals(value)){
+                    return index;
+                } else{
+                    current = current.prev;
+                    index --;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public String toString(){
+        Object[] completedlist = new Object[size];
+        Node current = head;
+        int index = 0;
+        while(index < size){
+            completedlist[index] = current.value;
+            current = current.next;
+            index++;
+        }
+        return Arrays.toString(completedlist);
+    }
+
+    class Node{
+        Object value;
+        Node next;
+        Node prev;
+
+        public Node(Object value){
+            this.value = value;
+        }
+    }
+
+    private void validateIndex(int index){
+        try {
+            if (index < 0 || index >= size){
+                throw new NullPointerException();
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("LinkedList size is " + size +
+                    ". Error: your index is bigger or equal than size or less than ZERO");
+        }
+    }
+
+    private void validateIndexForAdd(int index){
+        try {
+            if (index < 0 || index > size) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("LinkedList size is " + size +
+                    ". Error: your index is bigger than size or less than ZERO");
+        }
+    }
+}
---
+++ src/test/java/org/ogorodnik/datastructures/list/LinkedListTest.java
@@ -0,0 +1,161 @@
+package org.ogorodnik.datastructures.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    private LinkedList listWithZeroElements;
+    private LinkedList listWithOneElement;
+    private LinkedList listWithFourElements;
+    private LinkedList listWithFiveElements;
+
+    @Before
+    public void before() {
+        listWithZeroElements = new LinkedList();
+
+        listWithOneElement = new LinkedList();
+        char value = 'A';
+        listWithOneElement.add(value);
+
+        listWithFourElements = new LinkedList();
+        value = 'A';
+        for (int i = 0; i < 4; i++) {
+            listWithFourElements.add(value);
+            value++;
+        }
+
+        listWithFiveElements = new LinkedList();
+        value = 'A';
+        for (int i = 0; i < 5; i++) {
+            listWithFiveElements.add(value);
+            value++;
+        }
+    }
+
+        @Test
+        public void testAddWithZeroElements(){
+            char value = 'A';
+            listWithZeroElements.add(value);
+            assertEquals(1, listWithZeroElements.size());
+            assertEquals('A', listWithZeroElements.get(0));
+        }
+
+        @Test
+        public void testAddAfterTail(){
+            char value = 'E';
+            listWithFourElements.add(value);
+            assertEquals(5, listWithFourElements.size());
+            assertEquals('E', listWithFourElements.get(4));
+            assertEquals('D', listWithFourElements.get(3));
+        }
+
+        @Test
+        public void testAddBeforeHead(){
+            char value = 'E';
+            listWithFourElements.add(value, 0);
+            assertEquals(5, listWithFourElements.size());
+            assertEquals('E', listWithFourElements.get(0));
+            assertEquals('A', listWithFourElements.get(1));
+        }
+
+        @Test
+        public void testAddByIndex(){
+            char value = 'E';
+            listWithFourElements.add(value, 2);
+            assertEquals(5, listWithFourElements.size());
+            assertEquals('E', listWithFourElements.get(2));
+            assertEquals('B', listWithFourElements.get(1));
+            assertEquals('C', listWithFourElements.get(3));
+        }
+
+        @Test
+        public void testRemoveListWithOneElement(){
+        Object removed = listWithOneElement.remove(0);
+        assertEquals(0, listWithOneElement.size());
+        assertEquals('A',removed);
+        }
+
+        @Test
+        public void testRemoveHeadForListWithMoreThanOneElement(){
+        Object removed = listWithFourElements.remove(0);
+        assertEquals(3, listWithFourElements.size());
+        assertEquals('A', removed);
+        assertEquals('B', listWithFourElements.get(0));
+        }
+
+        @Test
+        public void testRemoveTailForListWithMoreThanOneElement(){
+        Object removed = listWithFourElements.remove(3);
+        assertEquals(3, listWithFourElements.size());
+        assertEquals('D', removed);
+        assertEquals('C', listWithFourElements.get(2));
+        }
+
+        @Test
+        public void testRemoveElementByIndex(){
+        Object removed = listWithFourElements.remove(2);
+        assertEquals(3, listWithFourElements.size());
+        assertEquals('C', removed);
+        assertEquals('B', listWithFourElements.get(1));
+        assertEquals('D', listWithFourElements.get(2));
+        }
+
+        @Test
+        public void testGet(){
+            char value = 'A';
+            for(int i=0; i<4; i++){
+                listWithFourElements.get(i);
+                assertEquals(value, listWithFourElements.get(i));
+                value++;
+            }
+        }
+
+        @Test
+        public  void testSet(){
+            Object setter = listWithFourElements.set('E', 2);
+            assertEquals(4, listWithFourElements.size());
+            assertEquals('E', setter);
+        }
+
+        @Test
+        public void testClear(){
+        listWithFourElements.clear();
+        assertTrue(listWithFourElements.isEmpty());
+        assertEquals(0, listWithFourElements.size());
+        }
+
+        @Test
+    public void testSize(){
+        assertEquals(4, listWithFourElements.size());
+        }
+
+        @Test
+    public void testIsEmpty(){
+        assertTrue(listWithZeroElements.isEmpty());
+        }
+
+        @Test
+    public void testContains(){
+        assertTrue(listWithFourElements.contains('B'));
+        }
+
+        @Test
+    public void testIndexOf(){
+        assertEquals(1, listWithFourElements.indexOf('B'));
+        }
+
+        @Test
+    public void testLastIndexOf(){
+        assertEquals(2, listWithFourElements.lastIndexOf('C'));
+        }
+
+        @Test
+    public void testToString(){
+        assertEquals("[A, B, C, D]", listWithFourElements.toString());
+    }
+}
+
+
